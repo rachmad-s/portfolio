d8d14a474622448a687646c859ce9715095bb8dd
rahmatsya <rachmadsyaefullah13@gmail.com>
2023-02-21 16:14:38 +0700

    create certification section

---
+++ src/components/certification.component.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { FiExternalLink } from 'react-icons/fi';
+
+import CSS from './../assets/certificates/css.png';
+import Py from './../assets/certificates/python.png';
+import ReactC from './../assets/certificates/react.png';
+import javascript2 from './../assets/certificates/javascript-2.png';
+
+export default function Certification() {
+  const certificates = [
+    {
+      name: 'CSS',
+      from: 'HackerRank',
+      description:
+        'It covers topics like exploring Cascading and Inheritance, exploring text styling fundamentals, understanding the use of layouts in CSS, understand the boxing of elements in CSS, among others.',
+      image: CSS,
+      link: 'https://www.hackerrank.com/certificates/09921b9ed268',
+      credentialId: '34DA6749CE5D'
+    },
+    {
+      name: 'Python',
+      from: 'HackerRank',
+      description:
+        'It covers topics like Scalar Types, Operators and Control Flow, Strings, Collections and Iteration, Modularity, Objects and Types and Classes',
+      image: Py,
+      link: 'https://www.hackerrank.com/certificates/195be9bba8d6',
+      credentialId: '34DA6749CE5D'
+    },
+    {
+      name: 'React',
+      from: 'HackerRank',
+      description:
+        'It covers topics like Basic Routing, Rendering Elements,State Management (Internal Component State), Handling Events, ES6 and JavaScript and Form Validation.',
+      image: ReactC,
+      link: 'https://www.hackerrank.com/certificates/5d81b00a0cec',
+      credentialId: '34DA6749CE5D'
+    },
+    {
+      name: 'Javascript (Intermediate)',
+      from: 'HackerRank',
+      description:
+        'It covers topics like Design Patterns, Memory management, concurrency model, and event loops, among others.',
+      image: javascript2,
+      link: 'https://www.hackerrank.com/certificates/ea2ad93ddda3',
+      credentialId: '34DA6749CE5D'
+    }
+  ];
+  return (
+    <section id="myWork">
+      <div className="container">
+        <h1 className="section-title text-center mb-5">Certifications</h1>
+        <div className="row">
+          {certificates.map((certificate) => (
+            <div className="col-6 certification-item" key={certificate.name}>
+              <div
+                className="d-flex align-items-center"
+                style={{ gap: '16px' }}
+              >
+                <img
+                  src={certificate.image}
+                  width="250"
+                  alt={`${certificate.name}-certification`}
+                />
+                <div>
+                  <div>
+                    <strong>
+                      {`${certificate.name}
+                      - 
+                      ${certificate.from}`}
+                    </strong>
+                  </div>
+                  <span className="font-weight-light">
+                    {certificate.description}
+                  </span>
+                  <br />
+                  <a href={certificate.link} className="btn btn-primary">
+                    Visit
+                    <FiExternalLink size={14} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import AboutMe from './../../components/about-me';
 import Header from './../../components/header.component';
 import MyWork from './../../components/my-work.component';
 import Navbar from './../../components/navbar.component';
+import Certification from '../../components/certification.component';
 
 export default function Home() {
   return (
@@ -11,9 +12,13 @@ export default function Home() {
       <Header />
       <AboutMe />
       <MyWork />
+      <Certification />
 
       <h1>&nbsp;</h1>
       <h1>&nbsp;</h1>
+      <p style={{ textAlign: 'center' }}>
+        More sections are still in progress... ;)
+      </p>
       <h1>&nbsp;</h1>
     </>
   );
