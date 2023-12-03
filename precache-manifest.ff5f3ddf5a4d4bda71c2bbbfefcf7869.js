cfa77cd453c89d9e2d13d822c0690b4802c42106
rsyaefullah <rsyaefullah@pvg.co.id>
2023-12-03 17:14:37 +0700

    Updates

---
+++ precache-manifest.ff5f3ddf5a4d4bda71c2bbbfefcf7869.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5fa065ee297e93e70e",
+    "url": "./static/js/main.fa5fa065.chunk.js"
+  },
+  {
+    "revision": "a40ea67f53c36dec64e4",
+    "url": "./static/js/1.a40ea67f.chunk.js"
+  },
+  {
+    "revision": "a6ab674e1f55c2a3c4b8",
+    "url": "./static/js/2.a6ab674e.chunk.js"
+  },
+  {
+    "revision": "a82fa439645721beb4b9",
+    "url": "./static/js/runtime~main.a82fa439.js"
+  },
+  {
+    "revision": "8d14c7a528ff767abb8700040e0de260",
+    "url": "./static/media/rect.8d14c7a5.svg"
+  },
+  {
+    "revision": "87a769cd27082bf68562e4ea8ae95ab1",
+    "url": "./static/media/logo-light-voffice.87a769cd.svg"
+  },
+  {
+    "revision": "c4d4f4fc3cb92e5c5eeba1b5c8888358",
+    "url": "./static/media/maybank-logo.c4d4f4fc.svg"
+  },
+  {
+    "revision": "c67f8fe96893f806a56f5c8004d897cd",
+    "url": "./static/media/logo-moladin.c67f8fe9.svg"
+  },
+  {
+    "revision": "b0d6e90f23cb4143b6c4b5aa94522d82",
+    "url": "./static/media/css.b0d6e90f.png"
+  },
+  {
+    "revision": "9148b322ee237df92d1c358f636456c2",
+    "url": "./static/media/python.9148b322.png"
+  },
+  {
+    "revision": "5aa3502e3a0c758706f983e6efb379aa",
+    "url": "./static/media/react.5aa3502e.png"
+  },
+  {
+    "revision": "212d061017910c4a8c32e6ef2e71e7c3",
+    "url": "./static/media/javascript-2.212d0610.png"
+  },
+  {
+    "revision": "178a6f36714b9f4829df58864ab3b4a5",
+    "url": "./static/media/anydesign.178a6f36.png"
+  },
+  {
+    "revision": "5f7d443cf4240101de725d9bd8166029",
+    "url": "./static/media/gnd.5f7d443c.png"
+  },
+  {
+    "revision": "d4301732ca3a50b476c8fa199bafa46b",
+    "url": "./static/media/handson.d4301732.png"
+  },
+  {
+    "revision": "4953c99602a9d2c2d69e818a54c1a0a4",
+    "url": "./static/media/helloka-1.4953c996.png"
+  },
+  {
+    "revision": "0bf0ba06c2655ef9af12d71df2c4797c",
+    "url": "./static/media/helloka-2.0bf0ba06.png"
+  },
+  {
+    "revision": "70c88faaa68430e2cdd7a905f7a4544e",
+    "url": "./static/media/hellolive-1.70c88faa.png"
+  },
+  {
+    "revision": "7e3f2632760eec6a274839e7d8831747",
+    "url": "./static/media/huqum-1.7e3f2632.png"
+  },
+  {
+    "revision": "b8bc0c17bbcb078ae2c07c5d8c6af7e1",
+    "url": "./static/media/izin.b8bc0c17.png"
+  },
+  {
+    "revision": "df5633392a4fa1b245586ccd7d01fafe",
+    "url": "./static/media/maybank-1.df563339.png"
+  },
+  {
+    "revision": "37b231d72b01756222b27f83f518fd4a",
+    "url": "./static/media/maybank-2.37b231d7.png"
+  },
+  {
+    "revision": "9d765ebc3c3c867a0b50220f2c0b7c5e",
+    "url": "./static/media/maybank-3.9d765ebc.png"
+  },
+  {
+    "revision": "29dc4294c6bbec9e7a1b375b2b6afb59",
+    "url": "./static/media/moladin.29dc4294.png"
+  },
+  {
+    "revision": "bb5d94205ff0564616e32794a4dddede",
+    "url": "./static/media/shopandbike.bb5d9420.png"
+  },
+  {
+    "revision": "ec6a5243539504c35593e8beb3b41940",
+    "url": "./static/media/voffice-1.ec6a5243.png"
+  },
+  {
+    "revision": "e6c9196a6a220b15e2fb091e4f14973c",
+    "url": "./static/media/voffice-2.e6c9196a.png"
+  },
+  {
+    "revision": "a3fd075297dd322ecb1fe7d92d125792",
+    "url": "./static/media/profile.a3fd0752.png"
+  },
+  {
+    "revision": "84b3eaa45dd451cfd9a8969e54281856",
+    "url": "./index.html"
+  }
+];
